c59f1858870e93b6cea7540d9c5582398d3db0ac
Izabela Soares <soares.izabela96@gmail.com>
2022-11-14 11:46:42 -0300

    adicionado components e roteamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule} from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
+
+// import { MatCardModule } from '@angular/material/card';
+// import { MatButtonModule} from '@angular/material/button';
+// import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
+import { HomeComponent } from './views/home/home.component';
+import { FeaturesComponent } from './views/features/features.component';
+import { PricingComponent } from './views/pricing/pricing.component';
+import { BlogComponent } from './views/blog/blog.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    FeaturesComponent,
+    PricingComponent,
+    BlogComponent,
+    ContactComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
-    MatIconModule,
     MatToolbarModule,
-    MatMenuModule,
-    MatButtonModule
-
+    NgbModule,
+    // MatMenuModule,
+    // MatCardModule,
+    // MatButtonModule,
+    // MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
