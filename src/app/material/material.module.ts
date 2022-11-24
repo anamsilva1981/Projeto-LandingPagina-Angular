1f92607b495d8080380d898c8679eeb9a58a47eb
Ana Maria <ana.msilva@hotmail.com>
2022-11-24 10:55:19 -0300

    criacao sessao blog

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -10,7 +10,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 
 
