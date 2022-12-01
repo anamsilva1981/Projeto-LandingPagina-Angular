25f8effbb02bab10573407a6ac26b13356b1ee59
Izabela Soares <soares.izabela96@gmail.com>
2022-12-01 15:28:06 -0300

    various fix

--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -6,6 +17,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  matcher = new MyErrorStateMatcher();
+  hide = true;
+
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  requiredFormControl = new FormControl('', [Validators.required]);
 
   constructor() { }
 
